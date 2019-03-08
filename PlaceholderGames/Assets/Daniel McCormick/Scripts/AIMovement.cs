5c8278f6ac6d2beddb742ab50765f2a68054c077
Dan <2mccod84@solent.ac.uk>
2019-03-08 20:06:14 +0000

    AI Update Commit
    - The AI can now travel through doors and close behind them (this can be modified to stop the player going through as well)
    - The AI looks around for anything tagged on the object layer and will look at it for a few seconds
    - If the AI cannot find anything near by it will randomly wander around the map!

--- PlaceholderGames/Assets/Daniel McCormick/Scripts/AIMovement.cs
+++ PlaceholderGames/Assets/Daniel McCormick/Scripts/AIMovement.cs
@@ -8,14 +8,17 @@ public class AIMovement : MonoBehaviour
     public Transform goal;
     private NavMeshAgent agent;
     private GameObject lastDoor;
-    public float doorTimer = 2f;
+    public float doorTimer = 0.5f;
+    public float browsingTimer = 5f;
     public bool openedDoor = false;
 
+    int layermask = 1 << 9;
+    Ray ray;
     // Start is called before the first frame update
     void Start()
     {
-         agent = GetComponent<NavMeshAgent>();
-        agent.destination = goal.position;
+        agent = GetComponent<NavMeshAgent>();
+       agent.destination = goal.position;
     }
 
     // Update is called once per frame
@@ -35,6 +38,48 @@ public class AIMovement : MonoBehaviour
                 doorTimer -= Time.deltaTime;
             }
         }
+        if(agent.remainingDistance < 1)
+        {
+            if (browsingTimer >= 0)
+            {
+                browsingTimer -= Time.deltaTime;
+            }
+            else
+            {
+                RaycastHit hit;
+                ray.origin = transform.position;
+                ray.direction = transform.TransformDirection(Vector3.forward);
+                for (int i = 0; i < 100; i++)
+                {
+                    if (Physics.SphereCast(ray, i, out hit, Mathf.Infinity, layermask, QueryTriggerInteraction.UseGlobal))
+                    {
+                        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+                        if (hit.transform.gameObject.name != goal.name)
+                        {
+                            goal = hit.transform;
+                            agent.destination = goal.position;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        agent.destination = WanderingNavAI(transform.position, 100, -1);
+                    }
+                }
+                browsingTimer = 5f;
+            }
+        }
+    }
+
+    Vector3 WanderingNavAI(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 direction = Random.insideUnitSphere * dist;
+        direction += origin;
+        NavMeshHit navHit;
+
+        NavMesh.SamplePosition(direction, out navHit, dist, layermask);
+
+        return navHit.position;
     }
 
     void OnCollisionEnter(Collision col)
