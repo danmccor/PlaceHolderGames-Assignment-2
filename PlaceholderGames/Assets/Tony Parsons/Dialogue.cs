132a2fd2bec923f782cc8bfd94b49e9a96103a2f
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-03-11 17:05:03 +0000

    Bath and Dialogue Script

---
+++ PlaceholderGames/Assets/Tony Parsons/Dialogue.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dialogue : MonoBehaviour
+{
+    public Text BoxToWriteIn;
+    //give them a thing to say
+    public string ThingToSay;
+    //make them say it
+    [HideInInspector]
+    public bool MakeTalk;
+    //get rid of the text box
+    [HideInInspector]
+    public bool Spoken;//character has said their line
+
+    private bool talking;//stop coroutine repeating
+
+    public float TalkSpeed; //rate at which letters appear on screen
+
+    private void Start()
+    {
+        MakeTalk = false;
+        Spoken = false;
+        talking = false;
+    }
+
+    private void Update()
+    {
+        //press F and they say the thing, this is for testing change the MakeTalk bool within a script to make them say stuff
+        if(Input.GetKeyDown(KeyCode.F) && !Spoken)
+        {
+            MakeTalk = true;
+        }
+        else if(Input.GetKeyDown(KeyCode.F) && Spoken)
+        {
+            MakeTalk = false;
+            talking = false;
+            Spoken = false;
+            BoxToWriteIn.text = "";
+        }
+        if(MakeTalk == true && !talking)
+        {
+            StartCoroutine(Talk());
+        }
+    }
+
+    private IEnumerator Talk()
+    {
+        talking = true;
+        //itterates through string reading out one character at a time to the text box
+        for (int i = 0; i < ThingToSay.Length + 1; i++)
+        {
+            BoxToWriteIn.text = ThingToSay.Substring(0, i);
+            if (i == ThingToSay.Length)
+                Spoken = true;
+            yield return new WaitForSeconds(TalkSpeed);
+        }
+        
+    }
+}
---
+++ PlaceholderGames/Assets/Tony Parsons/Underwater.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//THIS SCRIPT IS FOR THE BATHWATER, IT PUTS UP A BLUE PANEL 
+//WHEN THE CAMERA COLLIDES/ENTERS THE OBJECT
+//AND TURNS IT OFF WHEN THE CAMERA EXITS THE OBJECT
+public class Underwater : MonoBehaviour
+{
+    public GameObject bluePanel; //the panel to activated/deactivated
+    public ParticleSystem waterDrops;
+
+    private bool Wet;
+    private float delay;
+    private float countdown;
+
+    private void Start()
+    {
+        delay = 5;
+        countdown = delay;
+        Wet = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        bluePanel.SetActive(true);
+    }
+
+    //camera exits bath, switch off panel play particle effect for 3 seconds
+    private void OnTriggerExit(Collider other)
+    {
+        //switch off panel
+        bluePanel.SetActive(false);
+
+        //if the particle effect game object is off, switch it on
+        if (waterDrops.gameObject.activeSelf == false)
+            waterDrops.gameObject.SetActive(true);
+        //play the particle animation
+        waterDrops.Play();
+        Wet = true;
+    }
+
+    private void Update()
+    {
+        if(Wet)
+        {
+            countdown -= Time.deltaTime;
+            if (countdown <= 0)
+            {
+                waterDrops.gameObject.SetActive(false);
+                Wet = false;
+                countdown = delay;
+            }
+        }
+    }
+}
