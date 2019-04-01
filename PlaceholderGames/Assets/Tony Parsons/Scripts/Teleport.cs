e9e47931170d337dacb88b0437a1634903f9702e
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-04-01 04:28:34 +0100

    Portrait Event

---
+++ PlaceholderGames/Assets/Tony Parsons/Scripts/Teleport.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject Player;
+    //Portrait Morphing
+    public GameObject Normal_Portrait;//portrait that changes
+    public GameObject Morphing_Portrait;//portrait with changing animation
+    //Teleporting
+    public Transform Teleport_Spot;
+    private bool has_Teleported;
+    //Time Stuff
+    private float delay = 5.0f;
+    private float countdown;
+
+    private void Start()
+    {
+        Normal_Portrait.SetActive(false);
+        countdown = delay;
+        has_Teleported = false;
+    }
+
+    private void Update()
+    {
+        
+        countdown -= Time.deltaTime;
+
+        if (countdown <= 0)
+        {
+            //move player to transform, reset countdown, set bool == true
+            if (!has_Teleported)
+            {
+                Player.transform.position = Teleport_Spot.position;
+                countdown = delay;
+                has_Teleported = true;
+            }
+            //Change portrait back and de-activate this object
+            else
+            {
+                Normal_Portrait.SetActive(true);
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ PlaceholderGames/Assets/Tony Parsons/Scripts/Trigger_CamCorder.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger_CamCorder : MonoBehaviour
+{
+    public GameObject The_Event;
+    private bool isActive;
+
+    private void Start()
+    {
+        isActive = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!isActive)
+        {
+            The_Event.SetActive(true);
+            isActive = true;
+        }
+    }
+}
