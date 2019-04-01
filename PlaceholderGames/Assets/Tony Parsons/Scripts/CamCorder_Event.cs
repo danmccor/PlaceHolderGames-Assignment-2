72419e5d7ddb0ec98a27abbad5e361f6577fa567
Snoslraptor <33257135+Snoslraptor@users.noreply.github.com>
2019-04-01 05:37:50 +0100

    CamCorder Event (unfinished)
    I've done the exact same thing as I have with my test scene, but there's a problem here. I haven't been able to do this over the weekend, and it's 5:30AM.
    I'll sort it out tomorrow, sorry guys.

---
+++ PlaceholderGames/Assets/Tony Parsons/Scripts/CamCorder_Event.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamCorder_Event : MonoBehaviour
+{
+    //Camcorder
+    public GameObject CamCorder;
+    public GameObject CamCorder_Cutscene;
+    //Camera - As in player viewports
+    private CharacterController Plae;
+    public Camera CutSceneCam;
+    public Transform restoreCamPosition;//put camera here when it is re-activated
+    //time stuff
+    private float delay = 5.0f; //time animation takes
+    private float countdown;
+
+
+    //swap camcorder for one with animation
+    private void Start()
+    {
+        //deactivate player character and activate the cutscene camera
+        Plae = FindObjectOfType<CharacterController>();
+        Plae.gameObject.SetActive(false);
+        CamCorder.SetActive(false);
+        CamCorder_Cutscene.SetActive(true);
+        countdown = delay;
+    }
+
+    private void Update()
+    {
+        countdown -= Time.deltaTime;
+        //after 5 seconds
+        if(countdown <= 0)
+        {
+            //shut off cutscene cam, re-activate player
+            CamCorder_Cutscene.SetActive(false);
+            Plae.transform.position = restoreCamPosition.position;
+            Plae.gameObject.SetActive(true);
+        }
+    }
+
+}
